796da7630d4e65ea4bf4d7f2349c8057a66a41d3
Ross Marshall <rossmarshallgithub@gmail.com>
2017-01-20 23:40:52 -0500

    Core three

--- Tower_Of_Hanoi/tower_of_hanoi.py
+++ Tower_Of_Hanoi/tower_of_hanoi.py
@@ -5,54 +5,60 @@ import tower_disk
 
 class TowerOfHanoi(object):
 
-    #a = deque([])
-    #b = deque([])
-    #c = deque([])
-    a = PriorityQueue()
-    b = PriorityQueue()
-    c = PriorityQueue()
+    a = []
+    b = []
+    c = []
 
     moves = 0
     
     def __init__(self, n):
 	for x in range(1, n+1):
-	    self.a.put( tower_disk.Disk( x, 'A' ) )
+	    self.a.append( tower_disk.Disk( x, 'A' ) )
 
     def prettyPrint( self, t ):
         qLen = len( t )
+        counter = 1
         retStr = "[ "
-        for i in range( 1, qLen+1 ):
+        for i in t:
            retStr = retStr + str( i );
-           if i < qLen:
-              retStr = retStr + ","
+           counter += 1
+           if counter <= qLen:
+              retStr = retStr + ", "
         retStr = retStr + " ]"
         return retStr
         
 
     def moveDisks( self, s, d1, d2, nodeCount):
-        print "a ",s,"\n"
-        print "b ",d1,"\n"
-        print "c ",d2,"\n"
+        print "moveDisks a ",s,"\n"
+        print "moveDisks b ",d1,"\n"
+        print "moveDisks c ",d2,"\n"
         
-        d2.put( s.pop() )
+        d2.append( s[0] )
+        del s[0]
         self.moves += 1
         if nodeCount == 1:
            return;
         
-        d1.put( s.pop() )
-        d1.put( d2.pop() )
+        d1.append( s[0] )
+        del s[0]
+
+        d1.append( d2[0] )
+        del d2[0]
+
         self.moves += 2
         if nodeCount == 2:
            return;
 
-        d2.put( s.pop() )
-        s.put( d1.pop() )
-        d2.put( d1.pop() )
-        d2.put( s.pop() )
+        d2.append( s[0] )
+        del s[0]
+        s.append( d1[0] )
+        del d1[0]
+        d2.append( d1[0] )
+        del d1[0]
+        d2.append( s[0] )
+        del s[0]
         self.moves += 4
-        
-
-        
+            
 
 if __name__ == '__main__':
 
@@ -66,9 +72,9 @@ if __name__ == '__main__':
     print "b ",toh.b,"\n"
     print "c ",toh.c,"\n"
 
-    #print "pretty print a", toh.prettyPrint(toh.a)
-    #print "pretty print b", toh.prettyPrint(toh.b)
-    #print "pretty print c", toh.prettyPrint(toh.c)
+    print "pretty print a", toh.prettyPrint(toh.a)
+    print "pretty print b", toh.prettyPrint(toh.b)
+    print "pretty print c", toh.prettyPrint(toh.c)
 
 
     nodeCount = n % 3
