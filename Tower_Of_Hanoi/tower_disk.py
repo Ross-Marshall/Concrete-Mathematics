11834a6442e1886ed22fcd23f2cc58ba31a130c8
Ross Marshall <rossmarshallgithub@gmail.com>
2017-01-27 23:47:21 -0500

    Text only

--- Tower_Of_Hanoi/tower_disk.py
+++ Tower_Of_Hanoi/tower_disk.py
@@ -3,27 +3,26 @@ class Disk(object):
     tower    = ''
 
     # The class "constructor" - It's actually an initializer 
-    def __init__(self, id, tower):
+    def __init__(self, id):
         self.id       = id
-	self.tower    = tower
 
     def print_disk(self):
-       return "[ " + str(self.id) +" | " + self.tower + " ]"
+       return "[ " + str(self.id) + " ]"
 
     def pretty_print_disk(self):
-       return "[ " + str(self.id) +" | " + self.tower + " ]"
+       return "[ " + str(self.id) + " ]"
 
     def __str__(self):
        return str( self.id )
 
-def make_disk(id,tower):
-    disk = Disk(id,tower)
+def make_disk(id):
+    disk = Disk(id)
     return disk
 
 if __name__ == '__main__':
-    d = Disk(3,'A') 
+    d = Disk(3) 
     print d.print_disk()
 
-    d2 = make_disk( 6,'B' )
+    d2 = make_disk( 6 )
     print d2.print_disk()
  
--- Tower_Of_Hanoi/tower_of_hanoi.py
+++ Tower_Of_Hanoi/tower_of_hanoi.py
@@ -2,40 +2,11 @@ import sys
 from collections import deque
 from Queue import PriorityQueue
 import tower_disk
-import Stack
+from stack import Stack
 
 class TowerOfHanoi(object):
 
-    a =  None # Stack()
-    b =  None #Stack()
-    c =  None #nul #Stack()
-
-    aLength = 0
-    bLength = 0
-    cLength = 0
-
-    moves = 0
-    
-    def __init__(self, n):
-	for x in range(1, n+1):
-	    self.a.push( tower_disk.Disk( x ) )
-
-    def prettyPrint( self, t ):
-        qLen = len( t )
-        counter = 1
-        retStr = "[ "
-        for i in t:
-           retStr = retStr + str( i );
-           counter += 1
-           if counter <= qLen:
-              retStr = retStr + ", "
-        retStr = retStr + " ]"
-        return retStr
-
-    def refreshTowerLengths(self):
-        self.aLength = size( self.a )
-        self.bLength = size( self.b )
-        self.cLength = size( self.c )
+    moveCounter = 0;
 
     def moveTower(self, height, fromPole, toPole, withPole):
         if height >= 1:
@@ -43,60 +14,9 @@ class TowerOfHanoi(object):
             self.moveDisk(fromPole, toPole)
             self.moveTower(height-1, withPole, toPole, fromPole)
            
-    def moveDisk( self, fromPole, toPole ):
-        print "moving disk from ", self.prettyPrint(fromPole), " to ", self.prettyPrint(toPole) 
-        toPole.push( fromPole.pop() )
-        
-        
-    def checkTower(self, n, check):
-       if n <= check:
-           print "pretty print a", toh.prettyPrint(toh.a)
-           print "pretty print b", toh.prettyPrint(toh.b)
-           print "pretty print c", toh.prettyPrint(toh.c)
-           print "checkTower: Exiting..."
-           sys.exit()
-
-    def prettyPrintTowers(self):
-        print "pretty print a", self.prettyPrint(toh.a)
-        print "pretty print b", self.prettyPrint(toh.b)
-        print "pretty print c", self.prettyPrint(toh.c)
-
-    def moveDisks( self, s, d1, d2, nodeCount):
-        if len( s ) == 0:
-           d2.append( s[0] )
-        else:
-           d2.insert( 0, s[0] )
-        del s[0]
-
-        self.moves += 1
-        if nodeCount == 1:
-           return;
-        
-        d1.insert( 0, s[0] )
-        del s[0]
-
-        d1.insert( 0,  d2[0] )
-        del d2[0]
-
-        print "n =", n
-        self.moves += 2
-        if nodeCount == 2:
-           return;
-
-        d2.insert( 0,  s[0] )
-        del s[0]
-
-        s.insert( 0,  d1[0] )
-        del d1[0]
-
-        d2.insert( 0,  d1[0] )
-        del d1[0]
-
-        d2.insert( 0,  s[0] )
-        del s[0]
-
-        self.moves += 4
-            
+    def moveDisk( self, fp, tp ):
+        print "moving disk from ", fp, " to ", tp  
+        self.moveCounter += 1          
 
 if __name__ == '__main__':
 
@@ -111,18 +31,11 @@ if __name__ == '__main__':
          sys.exit(-1)
 
     
-    toh = TowerOfHanoi(n)
-    toh.prettyPrintTowers()
-    toh.a = Stack()
-    toh.b = Stack()
-    toh.c = Stack()
+    toh = TowerOfHanoi()
 
     if n % 2 == 1:
-        toh.moveTower( n, toh.a, toh.c, toh.b )
+        toh.moveTower( n, "A", "C", "B" )
     else:
-        toh.moveTower( n, toh.a, toh.b, toh.c )
+        toh.moveTower( n, "A", "B", "C" )
 
-    toh.prettyPrintTowers()
-
-    print "Done...\n"
-
+    print "Done...  Total number of moves = ",toh.moveCounter,"\n"
